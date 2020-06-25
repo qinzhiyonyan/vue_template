7bc11fe5bd4e12a721a3b21aaae81166a9bfd9f4
qinzhiyonyan <1447312816@qq.com>
2020-06-25 21:35:22 +0800

    父子组件传值的使用（props/$emit） 不同组件之间传值（$emit/$on）

---
+++ src/components/AShow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      {{ content }}
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'AShow',
+    props: {
+      content: {
+        type: [String, Number],
+        default:''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/store.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <a-input @input="handleInput"></a-input>
+    <a-show :content="inputValue"></a-show>
+  </div>
+</template>
+
+<script>
+
+  import AInput from '_c/AInput'
+  import AShow from '_c/AShow'
+
+  export default {
+    name: 'store',
+    data(){
+      return {
+        inputValue: ''
+      }
+    },
+    components: {
+      AInput,
+      AShow
+    },
+    methods: {
+      handleInput (val) {
+        this.inputValue = val
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
